8f99fdd888edef9b0949e12e37b93cb0eb6c31a9
BikeshKushwaha <121177672+BikeshKushwaha@users.noreply.github.com>
2024-03-02 21:53:37 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,135 @@
+//Navigation bar effects on scroll
+window.addEventListener("scroll", function(){
+const header = this.document.querySelector("header");
+header.classList.toggle("sticky", window.scrollY > 0);
+});
 
+//Services section - Modal
+const serviceModals= document.querySelectorAll(".service-modal");
+const learnmoreBtns= document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns= document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i) =>{
+    learnmoreBtn.addEventListener("click", () =>{
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
+
+//Portfolio section - Modal
+const portfolioModals= document.querySelectorAll(".portfolio-model");
+const imgCards= document.querySelectorAll(".img-card");
+const portfolioCloseBtns= document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function(modalClick){
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) =>{
+    imgCard.addEventListener("click", () =>{
+        portfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModals.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
+
+//Our clients - Swiper
+// var swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+
+//Website dark/light theme
+
+//Scroll to top button
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener("click", () =>{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+//Navigation menu items active on page scroll
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 50;
+        let id = current.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+        } 
+        else{
+            document.querySelector(".nav-items a[href*="+ id + "]").classList.remove("active"); 
+        }
+    });
+});
+
+//Responsive navigation menu toggle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelector(".nav-items a");
+ 
+menuBtn.addEventListener("click",() => {
+    navigation.classList.add("active");
+
+});
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+        navigation.classList.remove("active");
+    });
+});
+//Scroll reveal animations
+//Common reveal options to create reveal animations
+ScrollReveal({
+     reset: true,
+     distance: '60px',
+     duration: 2500,
+     delay: 100
+     });
+
+//Target elements, and specify options to create reveal animations
+ScrollReveal().reveal('.home .info h2', {delay: 500, origin: 'left'});
+ScrollReveal().reveal('.home .info h3 .home .info .p', {delay: 600, origin: 'right'});
+ScrollReveal().reveal('.section-title-01', { delay: 2000 });
